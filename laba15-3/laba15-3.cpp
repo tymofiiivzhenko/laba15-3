d7e3573f340eba09eb0f353968a98e6a45c35ccf
tymofiiivzhenko <tymofiiivzhenko@gmail.com>
2023-12-19 08:24:22 +0000

    Add project files.

---
+++ laba15-3/laba15-3.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i, N;
+
+    printf("Enter the size of the massif: ");
+    scanf("%d", &N);
+
+    int* A = (int*)malloc(N * sizeof(int));
+
+    printf("Enter the elements of the massif:\n");
+    for (i = 0; i < N; ++i) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &A[i]);
+    }
+
+    for (i = 0; i < N; ++i) {
+        if (A[i] % 2 == 0) {
+            A[i] = 0;
+        }
+    }
+
+    printf("New massif:\n");
+    for (i = 0; i < N; ++i) {
+        printf("%d\t", A[i]);
+    }
+    printf("\n");
+
+    free(A);
+
+    return 420;
+}
+
